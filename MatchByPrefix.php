bcb31584d4db2a39680238b531b1351c12353964
Vitaliy Filippov <vitalif@yourcmc.ru>
2011-12-16 15:14:24 +0000

    Bug 90854 - Follow redirects when prefix matches a redirect

--- MatchByPrefix.php
+++ MatchByPrefix.php
@@ -7,7 +7,7 @@
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
+ * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
@@ -45,6 +45,13 @@ function efMatchByPrefixMaybeRedirect(&$title, &$request, &$ignoreRedirect, &$ta
         {
             $row = $dbr->fetchObject($res);
             $target = Title::newFromRow($row);
+            if ($target->isRedirect())
+            {
+                $article = new Article($target);
+                $nt = $article->followRedirect();
+                if ($nt)
+                    $target = $nt;
+            }
             return false;
         }
     }
